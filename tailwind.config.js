74df858d4652fe23b0a563a1847848dcb9adf2ac
Trihartonoo <Triihartono29@gmail.com>
2022-04-22 22:35:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      height: {
+        140: '148vh',
+        150: '150vh',
+      },
+      width: {
+        94: '94%',
+        98: '98%',
+      },
+      fontFamily: {
+        sora: ['sora'],
+        poppins: ['poppins'],
+      },
+    },
+  },
+  plugins: [],
+};

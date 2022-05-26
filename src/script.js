0210980c7b904186e1e72f8686f542b47c19a1e0
Trihartonoo <Triihartono29@gmail.com>
2022-05-26 22:08:28 +0700

    navbar

---
+++ src/script.js
@@ -0,0 +1,19 @@
+const MenuBtn = document.querySelector('.Menu-btn');
+const MenuList = document.querySelector('.Menu-list');
+const MenuClose = document.querySelector('.Close-menu');
+const Navbar = document.querySelector('.Navbar');
+
+MenuBtn.addEventListener('click', () => {
+  MenuList.classList.toggle('Menu-list-active');
+});
+MenuClose.addEventListener('click', () => {
+  MenuList.classList.remove('Menu-list-active');
+});
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    Navbar.classList.add('Navbar-scroll');
+  } else {
+    Navbar.classList.remove('Navbar-scroll');
+  }
+});
